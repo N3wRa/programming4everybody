26355a93790b79eaf0be238cb1d6386947ae3b9d
N3wRa <riccardobernucci@gmail.com>
2020-04-07 14:13:28 +0200

    Add files via upload

---
+++ test/test_riccardobernucci.rb
@@ -0,0 +1,39 @@
+# commento singolo
+
+=begin
+questo è un commento
+=end
+
+print "Hello"
+puts "This is written in a new line"
+print "Still printing"
+
+print 1+3
+# Addition: output 4
+
+print 1-2
+# Subtraction: output -1
+
+print 9/3
+# Division: output 3
+
+print 2*3
+# Multiplication: output 6
+
+print 2**3
+# Exponentiation: output 8
+
+print 16%9
+# Modulo: output 7
+
+
+var = "codecademy"
+
+# Method to get the length of a string
+print var.length # 10
+
+# Method to get the string reversed
+print var.reverse # ymedacedoc
+
+# Method to conver all letters to uppercase
+print var.upcase # CODECADEMY
