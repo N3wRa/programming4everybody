f74df80947f35bfc32248ecf0905c6b7cef3d33e
N3wRa <riccardobernucci@gmail.com>
2020-04-07 14:18:36 +0200

    Update test_riccardobernucci.rb

--- test/test_riccardobernucci.rb
+++ test/test_riccardobernucci.rb
@@ -4,27 +4,41 @@
 questo è un commento
 =end
 
+print "<h3>step 1 test HTML title</h3>"
+
 print "Hello"
 puts "This is written in a new line"
 print "Still printing"
 
-print 1+3
+print 1+12
 # Addition: output 4
 
-print 1-2
+print 1-17
 # Subtraction: output -1
 
-print 9/3
+print 9/2
 # Division: output 3
 
-print 2*3
+print 2*5
 # Multiplication: output 6
 
-print 2**3
+print 2**8
 # Exponentiation: output 8
 
-print 16%9
+print 16%2
 # Modulo: output 7
+
+
+var = "codecademy"
+
+# Method to get the length of a string
+print var.length # 10
+
+# Method to get the string reversed
+print var.reverse # ymedacedoc
+
+# Method to conver all letters to uppercase
+print var.upcase # CODECADEMY
 
 
 var = "codecademy"
