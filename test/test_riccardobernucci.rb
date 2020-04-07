ac05f959fe48b9bd56c45ff12dbcc958df7de221
N3wRa <riccardobernucci@gmail.com>
2020-04-07 14:33:38 +0200

    Update test_riccardobernucci.rb

--- test/test_riccardobernucci.rb
+++ test/test_riccardobernucci.rb
@@ -1,43 +1,24 @@
-# commento singolo
-
-=begin
-questo è un commento
-=end
+# Put and print command
 
 print "Hello"
 puts "This is written in a new line"
 print "Still printing"
 
+# Arithmetic operations in Ruby
+
 print 1+12
-# Addition: output 4
 
 print 1-17
-# Subtraction: output -1
 
 print 9/2
-# Division: output 3
 
 print 2*5
-# Multiplication: output 6
 
 print 2**8
-# Exponentiation: output 8
 
 print 16%2
-# Modulo: output 7
 
-
-var = "codecademy"
-
-# Method to get the length of a string
-print var.length # 10
-
-# Method to get the string reversed
-print var.reverse # ymedacedoc
-
-# Method to conver all letters to uppercase
-print var.upcase # CODECADEMY
-
+# Ruby Object Methods
 
 var = "codecademy"
 
@@ -49,3 +30,31 @@ print var.reverse # ymedacedoc
 
 # Method to conver all letters to uppercase
 print var.upcase # CODECADEMY
+
+# Ruby Variables
+
+myVar = 48 
+
+# Strings in Ruby
+
+# String 1
+s1 = 'I am a single string!'
+
+# String 2
+s2 = "I am a double string!"
+
+# Boolean Data Types in Ruby
+
+# Boolean true variable
+year2019 = true
+
+# Boolean false variable
+year2018 = false
+
+# Numeric data types in Ruby
+
+# Integer value
+x = 1
+
+# Float value
+y = 1.2
